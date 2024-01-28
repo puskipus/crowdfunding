af1ab9a159676915844fed7749e0683fcfc83a5a
Thoriq Achmad <thoriqachmad.r@gmail.com>
2024-01-28 16:03:04 +0700

    init project and register api

---
+++ handler/user.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"crowdfunding/helper"
+	"crowdfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register failed", http.StatusUnprocessableEntity, "Error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+
+	if err != nil {
+		response := helper.APIResponse("Register failed", http.StatusBadRequest, "Error", nil)
+		c.JSON(http.StatusBadRequest, response)
+
+	}
+
+	formatter := user.FormatUser(newUser, "inijwtwoken")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "Success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
